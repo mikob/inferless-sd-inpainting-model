7d8451cb40c23b8630d11fb813966a26e5f18cc9
mikob <492864+mikob@users.noreply.github.com>
2024-02-14 12:54:50 -0600

    added input_schema

---
+++ input_schema.py
@@ -0,0 +1,74 @@
+INPUT_SCHEMA = {
+    "photo_id": {
+        'datatype': 'INT8',
+        'required': True,
+        'shape': [1],
+        'example': [0]
+    },
+    "mask_id": {
+        'datatype': 'INT8',
+        'required': True,
+        'shape': [1],
+        'example': [0]
+    },
+    "iteration_id": {
+        'datatype': 'INT8',
+        'required': True,
+        'shape': [1],
+        'example': [0]
+    },
+    "email": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["There is a fine house in the forest"]
+    },
+    "gallery_id": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["There is a fine house in the forest"]
+    },
+    "session": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["There is a fine house in the forest"]
+    },
+    "upload_id": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["There is a fine house in the forest"]
+    },
+    "prompt": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["There is a fine house in the forest"]
+    },
+    "negative_prompt": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["There is a fine house in the forest"]
+    },
+    "count": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["There is a fine house in the forest"]
+    },
+    "size": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["There is a fine house in the forest"]
+    },
+    "embed": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["There is a fine house in the forest"]
+    },
+}
